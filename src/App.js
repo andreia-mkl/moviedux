b715309a78de0332a1da2a2859768faf0d480bf9
andreia-mkl <izabela.vug@gmail.com>
2024-07-12 17:03:45 +0300

    feature/MDUX-003-movies-grid-component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import './styles.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MoviesGrid from './components/MoviesGrid';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
-      <Header></Header>
+        <Header></Header>
+        <MoviesGrid/>
       </div>
+      
       <Footer></Footer>
     </div>
   );
